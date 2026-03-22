c8a432c134e37d0ed82e0a44bae13657e326146f
neerajdara-dom <neerajdara.work@gmail.com>
2026-03-22 10:52:05 +0530

    Marks,Grading and Ranking system added

--- src/SMS.java
+++ src/SMS.java
@@ -1,9 +1,12 @@
+import java.sql.SQLOutput;
 import java.util.*;
 
 class Students{
     int id;
     String name;
     String course;
+    int marks;
+    char grade;
 }
 public class SMS{
     public static void main(String[] args) {
@@ -31,13 +34,46 @@ public class SMS{
                     obj.name=sc.nextLine();
                     System.out.println("Enter the course : ");
                     obj.course=sc.nextLine();
+                    System.out.println("Enter the marks /100 : ");
+                    obj.marks=sc.nextInt();
+                    if(obj.marks>=91&&obj.marks<=100){
+                        obj.grade='A';
+                    }else if(obj.marks>=81&&obj.marks<=90){
+                        obj.grade='B';
+                    }else if(obj.marks>=71&&obj.marks<=80){
+                        obj.grade='C';
+                    }else if(obj.marks>=61&&obj.marks<=70){
+                        obj.grade='D';
+                    }else if(obj.marks>=51&&obj.marks<=60){
+                        obj.grade='E';
+                    }else{
+                        obj.grade='F';
+                    }
+                    sc.nextLine();
                     studentsData.add(obj);
                 }
             }
             else if(choice==2){
                 for(Students s:studentsData){
-                    System.out.println(s.id+" "+s.name+" "+s.course);
+                    System.out.println(s.id+" "+s.name+" "+s.course+" "+s.marks);
                 }
+                System.out.println("Ranking System");
+                System.out.println("---------------");
+                for(Students p:studentsData){
+                    if(p.grade=='A'){
+                        System.out.println("1st Rank -> "+p.name+" "+p.id);
+                    }
+                    else if(p.grade=='B'){
+                        System.out.println("2nd Rank -> "+p.name+" "+p.id);
+                    }
+                    else if(p.grade=='C'){
+                        System.out.println("3rd Rank -> "+p.name+" "+p.id);
+                    }else{
+                        System.out.println("No Rank -> "+p.name+" "+p.id);
+                    }
+                }
+
+
             }
             else if(choice==3){
                 System.out.println("Enter the id : ");
@@ -45,7 +81,7 @@ public class SMS{
                 sc.nextLine();
                 for(Students s:studentsData){
                     if(s.id==searchId){
-                        System.out.println(s.id+" "+s.name+" "+s.course);
+                        System.out.println(s.id+" "+s.name+" "+s.course+" "+s.marks);
                     }
                     else{
                         System.out.println("Students data not stored !");
